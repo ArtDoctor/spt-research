b3deae01c6f766d69fabc062a860e9b16cd02c04
Ash <artdoctorka@gmail.com>
2024-05-08 12:18:42 +0200

    train model

--- set_venv.sh
+++ set_venv.sh
@@ -1,4 +1,5 @@
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda/nvvm/libdevice/libdevice.10.bc
